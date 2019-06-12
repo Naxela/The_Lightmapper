c9fe14179394b1412b716d89c7d9b5e52ba01dba
Alexander <KleemannAlexander@gmail.com>
2019-06-12 23:39:28 +0200

    Re-implement encoding algorithms

--- __init__.py
+++ __init__.py
@@ -452,6 +452,91 @@ class HDRLM_EncodeToRGBM(bpy.types.Operator):
 
         return {'FINISHED'}
 
+def encodeImageRGBM(self, image, maxRange, outDir):
+    input_image = bpy.data.images[image.name]
+    image_name = input_image.name
+
+    if input_image.colorspace_settings.name != 'Linear':
+        input_image.colorspace_settings.name = 'Linear'
+
+    # Removing .exr or .hdr prefix
+    if image_name[-4:] == '.exr' or image_name[-4:] == '.hdr':
+        image_name = image_name[:-4]
+
+    target_image = bpy.data.images.get(image_name + '_encoded')
+    print(image_name + '_encoded')
+    if not target_image:
+        target_image = bpy.data.images.new(
+                name = image_name + '_encoded',
+                width = input_image.size[0],
+                height = input_image.size[1],
+                alpha = True,
+                float_buffer = False
+                )
+    
+    num_pixels = len(input_image.pixels)
+    result_pixel = list(input_image.pixels)
+
+    for i in range(0,num_pixels,4):
+        for j in range(3):
+            result_pixel[i+j] *= 1.0 / maxRange;
+        result_pixel[i+3] = saturate(max(result_pixel[i], result_pixel[i+1], result_pixel[i+2], 1e-6))
+        result_pixel[i+3] = math.ceil(result_pixel[i+3] * 255.0) / 255.0
+        for j in range(3):
+            result_pixel[i+j] /= result_pixel[i+3]
+    
+    target_image.pixels = result_pixel
+    input_image = target_image
+    
+    #Save RGBM
+    input_image.filepath_raw = outDir + "_encoded.png"
+    input_image.file_format = "PNG"
+    input_image.save()
+
+def encodeImageRGBD(self, image, maxRange, outDir):
+    input_image = bpy.data.images[image.name]
+    image_name = input_image.name
+
+    if input_image.colorspace_settings.name != 'Linear':
+        input_image.colorspace_settings.name = 'Linear'
+
+    # Removing .exr or .hdr prefix
+    if image_name[-4:] == '.exr' or image_name[-4:] == '.hdr':
+        image_name = image_name[:-4]
+
+    target_image = bpy.data.images.get(image_name + '_encoded')
+    if not target_image:
+        target_image = bpy.data.images.new(
+                name = image_name + '_encoded',
+                width = input_image.size[0],
+                height = input_image.size[1],
+                alpha = True,
+                float_buffer = False
+                )
+    
+    num_pixels = len(input_image.pixels)
+    result_pixel = list(input_image.pixels)
+
+    for i in range(0,num_pixels,4):
+
+        m = saturate(max(result_pixel[i], result_pixel[i+1], result_pixel[i+2], 1e-6))
+        d = max(maxRange / m, 1)
+        d = saturate( math.floor(d) / 255 )
+
+        result_pixel[i] = result_pixel[i] * d * 255 / maxRange
+        result_pixel[i+1] = result_pixel[i+1] * d * 255 / maxRange
+        result_pixel[i+2] = result_pixel[i+2] * d * 255 / maxRange
+        result_pixel[i+3] = d
+    
+    target_image.pixels = result_pixel
+    
+    input_image = target_image
+
+    #Save RGBD
+    input_image.filepath_raw = outDir + "_encoded.png"
+    input_image.file_format = "PNG"
+    input_image.save()
+
 def draw(self, context):
     row = self.layout.row()
     row.label(text="Convert:")
@@ -860,7 +945,23 @@ def HDRLM_Build(self, context):
                     else:
                        print("Modules missing...") 
 
-                #Encode here
+                if scene.hdrlm_encoding_mode == "RGBM":
+                    encodeImageRGBM(self, bpy.data.images[obj.name+"_baked"], 6.0, bakemap_path)
+                    bpy.data.images[obj.name+"_baked"].name = obj.name + "_temp"
+                    bpy.data.images[obj.name+"_baked_encoded"].name = obj.name + "_baked"
+                    bpy.data.images.remove(bpy.data.images[obj.name+"_temp"])
+                elif scene.hdrlm_encoding_mode == "RGBD":
+                    encodeImageRGBD(self, bpy.data.images[obj.name+"_baked"], 6.0, bakemap_path)
+                    bpy.data.images[obj.name+"_baked"].name = obj.name + "_temp"
+                    bpy.data.images[obj.name+"_baked_encoded"].name = obj.name + "_baked"
+                    bpy.data.images.remove(bpy.data.images[obj.name+"_temp"])
+                
+
+                    #print()
+                    #bpy.data.images[obj.name+"_baked"].pixels = bpy.data.images[obj.name+"_baked"+"_Enctemp"].pixels
+                    #bpy.data.images[obj.name+"_baked"].name = obj.name + "_temp"
+
+                    #bpy.data.images[obj.name+"_baked"+"_Enctemp"].name = obj.name+"_baked"
 
     #Apply and restore materials
     for obj in bpy.data.objects:
@@ -890,6 +991,7 @@ def HDRLM_Build(self, context):
                     LightmapNode = nodetree.nodes.new(type="ShaderNodeTexImage")
                     LightmapNode.image = bpy.data.images[obj.name + "_baked"]
                     LightmapNode.name = "Lightmap_Image"
+                    LightmapNode.image.colorspace_settings.name = scene.hdrlm_encoding_colorspace
 
                     UVLightmap = nodetree.nodes.new(type="ShaderNodeUVMap")
                     UVLightmap.uv_map = "UVMap_Lightmap"
